0e40a1ea2ec911401921abd1b711c11070208265
anderson valencia <vaanderson06@gmail.com>
2024-04-09 19:34:27 -0500

    se sube archivo solicitud de datos

---
+++ 05-solicitud-datos.js
@@ -0,0 +1,12 @@
+const name = prompt ("ingrese su nombre")
+console.log(name);
+
+const num1 = prompt ("ingrese el primer numero")
+const num2= prompt ("ingrese el segundo numero")
+ num3 = num1*num2
+  console.log(num3);
+
+  const age = prompt ("ingrese su edad")
+  const añoact = prompt ("ingrese el año actual")
+   añonaci = añoact-age
+   console.log(añonaci); 
